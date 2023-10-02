2aa295e6f20a9469c22c659a9c0c4fcb5aa39921
Santiamaro <santiagofederico090@gmail.com>
2023-10-01 22:21:54 -0300

    Los datos ingresados se imprimen correctamente en la consola.

--- Json.js
+++ Json.js
@@ -1,25 +1,28 @@
-const nombre = document.getElementById("nombre").value;
-const apellido = document.getElementById("apellido").value;
-const fechaNacimiento = document.getElementById("fechaNacimiento").value;
+document.getElementById("registroForm").addEventListener("submit", (event)=> {
+    event.preventDefault(); 
 
-
-const datos = {
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const fechaNacimiento = document.getElementById("fechaNacimiento").value;
+    
+    const datos = {
 nombre: nombre,
 apellido: apellido,
 fechaNacimiento: fechaNacimiento
-};
-
-fetch("https://jsonplaceholder.typicode.com/users", {
+    };
+    
+    fetch("https://jsonplaceholder.typicode.com/users", {
 method: "POST",
 headers: {
-    "Content-Type": "application/json"
+        "Content-Type": "application/json"
 },
 body: JSON.stringify(datos)
-})
-.then(response => response.json())
-.then(data => {
-    console.log(datos);
-})
-.catch(error => {
-    console.error("Error:", error);
+    })
+    .then(response => response.json())
+    .then(data => {
+console.log(data);
+    })
+    .catch(error => {
+console.error("Error:", error);
+    });
 });
